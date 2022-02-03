35667d4758ac73a8bc54f2ffc2ce424eec5c5408
Ali Hashemi <ali00h@gmail.com>
2022-02-03 12:56:05 +0330

    add datasets

---
+++ codes/SupportResistanceCalculator.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace support_resistance_csharp.codes
+{
+    class SupportResistanceCalculator
+    {
+        private List<Double> line;
+        public SupportResistanceCalculator(List<Double> line){
+            this.line = line;
+        }
+
+        private void Calculate(){
+
+        }
+
+    }
+}
